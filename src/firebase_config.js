2e03716fa211655bfc0733ce6630a6cd6a2435a6
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-07-27 13:16:30 +0200

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "firebase/app";
-import {getFirestore} from '@firebase/firestore'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyArmqrZDCxImMZXsssbeHwZMBPEf_u93kk",
-    authDomain: "fir-tut-1c930.firebaseapp.com",
-    projectId: "fir-tut-1c930",
-    storageBucket: "fir-tut-1c930.appspot.com",
-    messagingSenderId: "694490625949",
-    appId: "1:694490625949:web:1102e81106dba1187fe174",
-    measurementId: "G-2NGT61JBSD"
-  };
-
-  const app = initializeApp(firebaseConfig) 
-  export const db = getFirestore(app)
